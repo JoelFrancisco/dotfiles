62908048e89ef7313db68671fd997e1dd594c3e8
Joel Francisco <joelfranciscofilho@gmail.com>
2026-03-23 01:14:40 -0300

    Fix noto-fonts-emoji -> noto-fonts-color-emoji (renamed in nixpkgs)

--- modules/desktop/fonts.nix
+++ modules/desktop/fonts.nix
@@ -5,7 +5,7 @@
       packages = with pkgs; [
         noto-fonts
         noto-fonts-cjk-sans
-        noto-fonts-emoji
+        noto-fonts-color-emoji
         nerd-fonts.jetbrains-mono
         font-awesome
       ];
