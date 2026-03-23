e6fc2dcc2deb1a4e4d95981e3a3b3e675726b6f4
Joel Francisco <joelfranciscofilho@gmail.com>
2026-03-23 01:04:06 -0300

    Fix flake-parts missing systems definition

--- flake.nix
+++ flake.nix
@@ -14,6 +14,8 @@
   };
 
   outputs = inputs:
-    inputs.flake-parts.lib.mkFlake { inherit inputs; }
-      (inputs.import-tree ./modules);
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "x86_64-linux" ];
+      imports = [ (inputs.import-tree ./modules) ];
+    };
 }
